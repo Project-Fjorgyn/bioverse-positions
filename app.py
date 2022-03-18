d2eb0e5464f7d74ea9741089430d7df860e5cd64
erebuseternal <marcelsanders96@gmail.com>
2022-03-18 20:58:57 +0000

    heroku db

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+import os
 from flask import Flask
 from flask_restful import Api
 
 from resources.location import Location, Locations
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 app.config['PROPOGATE_EXCEPTIONS'] = True
 api = Api(app)

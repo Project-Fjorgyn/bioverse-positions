e560829416b8c20939604e3de31d53bfeaa17155
erebuseternal <marcelsanders96@gmail.com>
2022-03-18 21:06:37 +0000

    whoops

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 db_uri = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 if db_uri.startswith('postgres://'):
     db_uri = 'postgresql' + db_uri[8:]
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 app.config['PROPOGATE_EXCEPTIONS'] = True
 api = Api(app)
